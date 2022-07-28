07ae79cdfeaad03adc5c8786702e31ba2a0365d5
simonajones <simonajones@gmail.com>
2022-07-28 23:03:27 +0100

    Progress bar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import RequireData from './common/requireData';
-import Layout from './components/views/layouts/layout';
+
 import MemberLayout from './components/views/layouts/memberLayout';
 import { clearMembers } from './services/memberData';
 import { clearMembership } from './services/membershipData';
 //
+import Layout from './components/views/layouts/layout';
+import Progress from './components/progress';
 import Agent from './components/route/agent';
 import Membership from './components/route/membership';
 import Index from './components/route/index';
@@ -16,11 +18,16 @@ import MemberEdit from './components/route/memberEdit';
 import MemberAdd from './components/route/memberAdd';
 import ConditionAdd from './components/route/conditionAdd';
 import ConditionEdit from './components/route/conditionEdit';
+import { getAgent } from './services/agentData';
+import { getMembership } from './services/membershipData';
+import { memberCount } from './services/memberData';
 
 import { submit } from './services/submit';
 
 const App = () => {
 	const [messageState, setMessageState] = useState([]);
+	const [selectedMember, setSelectedMember] = useState(null);
+	const [progress, setProgress] = useState(1);
 
 	function addMessage(messageText) {
 		const message = {
@@ -33,6 +40,26 @@ const App = () => {
 		setMessageState(messages);
 	}
 
+	function updateProgress() {
+		if (getAgent) {
+			setProgress(33);
+		}
+		if (getMembership().membershipNumber) {
+			setProgress(66);
+		}
+		if (memberCount() > 0) {
+			setProgress(100);
+		}
+	}
+
+	function callbackSelectedMember(member) {
+		console.log('got member', member);
+		if (member !== selectedMember) {
+			console.log('setting member', member);
+			setSelectedMember(member);
+		}
+	}
+
 	function callbackMessageDelete(messageId) {
 		const messages = messageState.filter(
 			(_message) => _message.id !== messageId
@@ -43,6 +70,7 @@ const App = () => {
 	function callbackClearForm() {
 		clearMembers();
 		clearMembership();
+		updateProgress();
 		addMessage('The form has been reset');
 	}
 
@@ -54,18 +82,23 @@ const App = () => {
 
 	return (
 		<>
+			<Progress amount={progress} />
 			<Routes>
 				<Route
 					element={
 						<Layout
+							progress={progress}
 							messageState={messageState}
 							callbackMessageDelete={callbackMessageDelete}
 						/>
 					}
 				>
 					<Route path="/" element={<Index />} />
-					<Route path="agent" element={<Agent />} />
-					<Route path="membership" element={<Membership />} />
+					<Route path="agent" element={<Agent postback={updateProgress} />} />
+					<Route
+						path="membership"
+						element={<Membership postback={updateProgress} />}
+					/>
 				</Route>
 
 				<Route
@@ -73,6 +106,7 @@ const App = () => {
 					element={
 						<RequireData>
 							<MemberLayout
+								progress={progress}
 								callbackSubmitForm={callbackSubmitForm}
 								callbackClearForm={callbackClearForm}
 								messageState={messageState}
@@ -81,10 +115,18 @@ const App = () => {
 						</RequireData>
 					}
 				>
-					<Route index element={<MemberIndex />} />
-					<Route path="add" element={<MemberAdd />} />
+					<Route index element={<MemberIndex postback={updateProgress} />} />
+					<Route path="add" element={<MemberAdd postback={updateProgress} />} />
 					<Route path="edit/:member" element={<MemberEdit />} />
-					<Route path="view/:member" element={<MemberView />} />
+					<Route
+						path="view/:member"
+						element={
+							<MemberView
+								callbackSetSelected={callbackSelectedMember}
+								postback={updateProgress}
+							/>
+						}
+					/>
 					<Route path="add-condition/:member" element={<ConditionAdd />} />
 					<Route
 						path="edit-condition/:condition/:member"
